ce52c980bac1229efdaa7333522884bf3f4e8995
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-05-04 18:57:17 +0300

    not-found and error pages added

---
+++ app/meals/error.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+
+const ErrorPage = () => {
+  return (
+    <main className='error'>
+        <h1>An Error Occurred.</h1>
+        <p>Failed to fetch meal data. Please try again.</p>
+    </main>
+  )
+}
+
+export default ErrorPage
---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main>
+        <h1>Not found</h1>
+        <p>Unfortunately, we could not find what you were looking for.</p>
+    </main>
+  )
+}
+
+export default NotFound
