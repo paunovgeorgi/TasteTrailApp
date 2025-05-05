260452953ad80a3b16d056fd3311872546f21637
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-05-05 10:03:27 +0300

    all-meals page implemented

---
+++ app/meals/loading-out.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './loading.module.css';
+
+const MealsLoadingPage = () => {
+  return (
+    <p className={classes.loading}>Fething items...</p>
+  )
+}
+
+export default MealsLoadingPage
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import classes from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+
+async function Meals(){
+  const meals = await getMeals();
+  return <MealsGrid meals={meals}/>;
+}
 
 const MealsPage = () => {
+
+
   return (
-    <h1>Meals Page</h1>
+    <>
+      <header className={classes.header}>
+        <h1>Delicuius meals, created {' '} 
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It's easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fething items...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   )
 }
 
