f4f2c0c90e3a15825eb6fb011e86ab3e572d3210
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-05-05 19:18:31 +0300

    server-side validations added for meal creation

---
+++ app/meals/share/error.js
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+
+
+const ErrorPage = () => {
+  return (
+    <main className='error'>
+        <h1>An Error Occurred.</h1>
+        <p>Failed to create meal.</p>
+    </main>
+  )
+}
+
+export default ErrorPage
